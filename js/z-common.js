22ab6d44d218b2e60a513762beabaac535b24511
Лариса <dlis0608@gmail.com>
2020-06-15 03:30:00 +0300

    Написана разметка для главной, стили для блока Header

---
+++ js/z-common.js
@@ -0,0 +1,21 @@
+$(".production-slider").slick({
+
+    infinite: true,
+
+    responsive: [{
+
+        breakpoint: 1024,
+        settings: {}
+
+    }, {
+
+        breakpoint: 600,
+        settings: {}
+
+    }, {
+
+        breakpoint: 300,
+        settings: {}
+
+    }]
+});
